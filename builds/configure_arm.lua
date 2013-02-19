9e9256da7746bcd68c090f85029e6318430e296b
Mark Pupilli <>
2013-02-19 19:51:57 +0000

    Fix path to new beagle deloyment.

--- builds/configure_arm.lua
+++ builds/configure_arm.lua
@@ -3,9 +3,11 @@
 
 TARGET_DIR   = 'beagle_build_g++4.6'
 
-ARM_DEPLOYMENT = '/home/mark/custom_bb_kernel/deploy'
 TOOLCHAIN = 'arm-linux-gnueabi-'
 LIB_TYPE = 'SharedLib'
+
+ARM_PACKAGES = '/home/mark/beagle_kernel_2.6.32/packages'
+ARM_DEPLOYMENT = '/home/mark/beagle_kernel_2.6.32/deploy'
 
 INCLUDE_DIRS = {
     '../../glk/include',
@@ -13,18 +15,20 @@ INCLUDE_DIRS = {
     ARM_DEPLOYMENT .. '/include',
     ARM_DEPLOYMENT .. '/include/unicap',
     ARM_DEPLOYMENT .. '/include/freetype2',
-    '/home/mark/custom_bb_kernel/packages/lua-5.1.4/src'
+    ARM_PACKAGES .. '/lua-5.1.4/src'
 }
 
 LIB_DIRS = {
     '../../glk/premake/beagle_build_g++4.6/debug',
     '../../glk/premake/beagle_build_g++4.6/release',
-    ARM_DEPLOYMENT .. '/lib', '/home/mark/custom_bb_kernel/packages/lua-5.1.4/src'
+    ARM_DEPLOYMENT .. '/lib',
+    ARM_PACKAGES .. '/lua-5.1.4/src',
+    ARM_DEPLOYMENT .. '/usr/local/lib'
 }
 
 DEFINES = { 'ARM_BUILD','__STDC_CONSTANT_MACROS', '__STDC_LIMIT_MACROS' }
 BUILD_OPTIONS = { '-std=c++0x -static -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp --sysroot=' .. ARM_DEPLOYMENT }
-LINK_OPTIONS = { '-Wl,--allow-shlib-undefined,-rpath=/usr/local/lib:' .. ARM_DEPLOYMENT }
+LINK_OPTIONS = { '-Wl,--allow-shlib-undefined,-rpath=/usr/local/lib' }
 
 OPENCV_LINKS = { 'opencv_core', 'opencv_contrib' }
 OPENGL_LINKS = {}
