a4d961a194caa46e04e2610ebaa502f12078c5c2
Mark Pupilli <>
2013-10-20 15:58:57 +0100

    portable serialization for timespec structures.

--- src/io/Serialisation.h
+++ src/io/Serialisation.h
@@ -1,5 +1,5 @@
 /**
-
+    Copyright (C) Mark Pupilli 2013, All Rights Reserved
     This file contains utilities that wrap up functionality from the Cereal library.
 
     Also contains cereal compatible serialize functions for some common types.
@@ -9,18 +9,10 @@
 
 #include <time.h>
 
-#include "../io/VectorStream.h"
 #include <cereal/archives/portable_binary.hpp>
 
-template<typename T>
-void serialize(T& archive, timespec& t)
-{
-    /// @todo - this is probably not portable
-    archive(t.tv_sec,t.tv_nsec);
-}
-
 template<typename ...Args>
-void Serialise(VectorOutputStream& stream, const Args&... types)
+void Serialise(std::streambuf& stream, const Args&... types)
 {
     std::ostream archiveStream(&stream);
     cereal::PortableBinaryOutputArchive archive(archiveStream);
@@ -28,11 +20,29 @@ void Serialise(VectorOutputStream& stream, const Args&... types)
 }
 
 template<typename ...Args>
-void Deserialise(VectorInputStream& vis, Args&... types)
+void Deserialise(std::streambuf& vis, Args&... types)
 {
     std::istream stream(&vis);
     cereal::PortableBinaryInputArchive archive(stream);
     archive(std::forward<Args&>(types)...);
+}
+
+template<typename T>
+void save(T& archive, const timespec& t)
+{
+    const int64_t sec = t.tv_sec;
+    const int64_t nsec = t.tv_nsec;
+    archive(sec,nsec);
+}
+
+template<typename T>
+void load(T& archive, timespec& t)
+{
+    int64_t sec;
+    int64_t nsec;
+    archive(sec,nsec);
+    t.tv_sec = sec;
+    t.tv_nsec = nsec;
 }
 
 #endif // SERIALISATION_H
