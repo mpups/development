640cd32edb3ecb34e77ede694a5bd187b396aad9
Mark Pupilli <>
2013-11-10 15:51:08 +0000

    Change build for new kernel and compiler.

--- builds/configure_arm.lua
+++ builds/configure_arm.lua
@@ -1,15 +1,14 @@
 -- You can change these variables so that
 -- they are appropriate for your system:
 
-TARGET_DIR   = 'beagle_build_g++4.6'
+TARGET_DIR   = 'beagle_build_g++4.8'
 
-ARM_PACKAGES = '/home/mark/beagle_kernel_2.6.32/packages'
-ARM_DEPLOYMENT = '/home/mark/beagle_kernel_2.6.32/deploy'
+ARM_PACKAGES = '/home/mark/beagle_kernel_3.7/packages'
+ARM_DEPLOYMENT = '/home/mark/beagle_kernel_3.7/deploy'
 TOOLCHAIN = 'arm-linux-gnueabi-'
 LIB_TYPE = 'SharedLib'
 
 INCLUDE_DIRS = {
-    ARM_PACKAGES .. '/gtest-1.6.0/include',
     ARM_DEPLOYMENT .. '/include',
     ARM_DEPLOYMENT .. '/include/unicap',
 }
@@ -20,7 +19,7 @@ LIB_DIRS = {
 
 DEFINES = { 'ARM_BUILD','__STDC_CONSTANT_MACROS', '__STDC_LIMIT_MACROS' }
 BUILD_OPTIONS = { '--sysroot=/usr/arm-linux-gnueabi -std=c++0x -static -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp' }
-LINK_OPTIONS = { '-Wl,--allow-shlib-undefined,-rpath=/usr/local/lib' }
+LINK_OPTIONS = { '-Wl,--allow-shlib-undefined,-rpath=/lib:/usr/local/lib' }
 
 FFMPEG_LINKS = { 'avformat', 'avcodec', 'avutil', 'swscale' }
 LINKS = { 'unicap' }
