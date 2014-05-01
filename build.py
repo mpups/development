b7726e9b002a390c077719827ef112118e7bbf52
Mark Pupilli <>
2014-05-01 17:26:51 +0100

    Add default args for build methods.

--- build.py
+++ build.py
@@ -2,17 +2,18 @@ import os
 import utils
 
 # Build an executable
-def Program( ENV, NAME, CPPPATH, LIBS, RPATH, SRC, SUPPORTED_PLATFORMS ):
+def Program( ENV, NAME, SRC, SUPPORTED_PLATFORMS, CPPPATH='', LIBS='', LIBPATH='', RPATH='' ):
     if not utils.TargetIsSupported(ENV['platform'],SUPPORTED_PLATFORMS):
         return None
     env = ENV.Clone()
     env.Append( CPPPATH=CPPPATH )
     env.Append( LIBS=LIBS )
+    env.Append( LIBPATH=LIBPATH )
     env.Append( LINKFLAGS = [ '-Wl,-rpath=' + RPATH ] )
     return env.Program( target=NAME, source=SRC )
 
 # Build a shared library
-def SharedLibrary( ENV, NAME, CPPPATH, LIBS, LIBPATH, RPATH, SRC, SUPPORTED_PLATFORMS ):
+def SharedLibrary( ENV, NAME, SRC, SUPPORTED_PLATFORMS, CPPPATH='', LIBS='', LIBPATH='', RPATH='' ):
     if not utils.TargetIsSupported(ENV['platform'],SUPPORTED_PLATFORMS):
         return None
     env = ENV.Clone()
